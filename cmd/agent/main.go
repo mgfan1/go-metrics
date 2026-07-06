33a36a842a13cfee698edbae3c0d62e6d921b5bf
Marat Gaifutdinov <gaifutdinov@mego.travel>
2026-07-06 17:21:15 +0700

    Revert "Инкремент 2: агент сбора рантайм-метрик"
    This reverts commit 47902d841e9aff934ba8e6f8ff1207c62772bfd6.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,11 +1,3 @@
 package main
 
-import (
-	"time"
-
-	"github.com/mgfan1/go-metrics/internal/agent"
-)
-
-func main() {
-	agent.New("localhost:8080", 2*time.Second, 10*time.Second).Run()
-}
+func main() {}
