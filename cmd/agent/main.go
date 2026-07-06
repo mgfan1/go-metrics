46daa665bc902adeb0a07c6f7d7f7068bf9eb422
Marat Gaifutdinov <gaifutdinov@mego.travel>
2026-07-06 17:19:36 +0700

    Инкремент 2: агент сбора рантайм-метрик

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import (
+	"time"
+
+	"github.com/mgfan1/go-metrics/internal/agent"
+)
+
+func main() {
+	agent.New("localhost:8080", 2*time.Second, 10*time.Second).Run()
+}
